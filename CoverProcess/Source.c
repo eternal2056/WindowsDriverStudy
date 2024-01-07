9ab1d0025d5362e90e834b06aa37afff067fb4fc
Eternal <34774347+eternal2056@users.noreply.github.com>
2024-01-07 23:12:02 +0800

    [NEW] PROJECT

---
+++ CoverProcess/Source.c
@@ -0,0 +1,19 @@
+﻿#include <ntifs.h>
+
+
+VOID DriverUnload(DRIVER_OBJECT* DriverObject) {
+	DbgPrintEx(77, 0, "DriverUnload\n");
+}
+
+NTSTATUS DriverEntry(
+	IN PDRIVER_OBJECT DriverObject,
+	IN PUNICODE_STRING RegistryPath
+)
+{
+	DriverObject->DriverUnload = DriverUnload;
+	DbgPrintEx(77, 0, "DriverEntry\n");
+	NTSTATUS status;
+	status = STATUS_SUCCESS;
+
+	return status;
+}
