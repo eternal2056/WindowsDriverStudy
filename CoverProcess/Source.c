9ee6c63e3b10084cd0f2934414582c510a63dbce
Eternal <34774347+eternal2056@users.noreply.github.com>
2024-01-12 00:01:49 +0800

    [UPDATE] PRINT.PROCESS.NAME

--- CoverProcess/Source.c
+++ CoverProcess/Source.c
@@ -1,8 +1,29 @@
 ﻿#include <ntifs.h>
 
-
+NTKERNELAPI
+NTSTATUS PsLookupProcessByProcessId(
+	__in HANDLE ProcessId,
+	__deref_out PEPROCESS* Process
+);
+NTKERNELAPI
+UCHAR*
+PsGetProcessImageFileName(
+	__in PEPROCESS Process
+);
 VOID DriverUnload(DRIVER_OBJECT* DriverObject) {
 	DbgPrintEx(77, 0, "DriverUnload\n");
+}
+
+VOID GetProcess() {
+	PEPROCESS Process = NULL;
+	PsLookupProcessByProcessId(3944, &Process);
+	DbgPrintEx(77, DPFLTR_ERROR_LEVEL, "Process %p \n", Process);
+	UCHAR* ImageName = PsGetProcessImageFileName(Process);
+	DbgPrintEx(77, DPFLTR_ERROR_LEVEL, "Process %p \n", ImageName);
+	DbgPrintEx(77, DPFLTR_ERROR_LEVEL, "Process %s \n", ImageName);
+	if (Process) {
+		ObDereferenceObject(Process);
+	}
 }
 
 NTSTATUS DriverEntry(
@@ -12,6 +33,8 @@ NTSTATUS DriverEntry(
 {
 	DriverObject->DriverUnload = DriverUnload;
 	DbgPrintEx(77, 0, "DriverEntry\n");
+	DbgBreakPoint();
+	GetProcess();
 	NTSTATUS status;
 	status = STATUS_SUCCESS;
 
