4f9fdcf9f16afb9f4562d0fac502b79a6664b514
Eternal <34774347+eternal2056@users.noreply.github.com>
2024-01-21 23:54:09 +0800

    [NEW] FileMiniFilterDriver

---
+++ FileMiniFilterUserDll/pch.h
@@ -0,0 +1,39 @@
+﻿// pch.h: This is a precompiled header file.
+// Files listed below are compiled only once, improving build performance for future builds.
+// This also affects IntelliSense performance, including code completion and many code browsing features.
+// However, files listed here are ALL re-compiled if any one of them is updated between builds.
+// Do not add files here that you will be updating frequently as this negates the performance advantage.
+
+#ifndef PCH_H
+#define PCH_H
+
+// add headers that you want to pre-compile here
+#include "framework.h"
+#include "windows.h"
+#include <stdio.h>
+#include <FltUser.h>
+
+#pragma comment(lib, "user32.lib")
+#pragma comment(lib, "kernel32.lib")
+#pragma comment(lib, "fltLib.lib")
+//#pragma comment(lib, "fltMgr.lib")
+//#pragma comment(lib, "ntoskrnl.lib")
+#pragma comment(lib, "hal.lib")
+
+extern HANDLE g_hPort;
+
+#define NPMINI_NAME            L"FileMiniFilterDriver"
+#define NPMINI_PORT_NAME       L"\\NPMiniPort"
+
+__declspec(dllexport)	int InitialCommunicationPort(void);
+__declspec(dllexport)   int NPSendMessage(PVOID InputBuffer);
+
+typedef enum _NPMINI_COMMAND {
+	ENUM_PASS = 0,
+	ENUM_BLOCK
+} NPMINI_COMMAND;
+
+typedef struct _COMMAND_MESSAGE {
+	NPMINI_COMMAND 	Command;
+} COMMAND_MESSAGE, * PCOMMAND_MESSAGE;
+#endif //PCH_H
---
+++ FileMiniFilterUserExe/FileMiniFilterUserExe.cpp
@@ -0,0 +1,80 @@
+﻿#include "FileMiniFilterUserExe.h"
+#include <iostream>
+using namespace std;
+
+int(__stdcall* pNPSendMessage)(PVOID pInBuffer);
+int(__stdcall* pInitialCommunicationPort)(VOID);
+
+CNPApp::CNPApp()
+{
+	m_hModule = NULL;
+	LoadNPminifilterDll();
+}
+
+CNPApp::~CNPApp()
+{
+	if (m_hModule) {
+		FreeLibrary(m_hModule);
+	}
+}
+bool CNPApp::LoadNPminifilterDll(void)
+{
+	m_hModule = LoadLibrary("FileMiniFilterUserDll.dll");
+	printf("USER: m_hModule: %p\n", m_hModule);
+	printf("USER: LoadNPminifilterDll\n");
+	if (m_hModule != NULL) {
+		printf("USER: LoadNPminifilterDll, FileMiniFilterUserDll: %p\n", GetModuleHandle("FileMiniFilterUserDll.dll"));
+		printf("USER: LoadNPminifilterDll, NPSendMessage: %p\n", GetProcAddress(::GetModuleHandle("FileMiniFilterUserDll.dll"), "NPSendMessage"));
+		printf("USER: LoadNPminifilterDll, InitialCommunicationPort: %p\n", GetProcAddress(::GetModuleHandle("FileMiniFilterUserDll.dll"), "InitialCommunicationPort"));
+		printf("USER: LoadNPminifilterDll, LoadLibraryA: %p\n", GetProcAddress(::GetModuleHandle("kernel32.dll"), "LoadLibraryA"));
+		pNPSendMessage = (int(__stdcall*)(PVOID)) GetProcAddress(GetModuleHandle("FileMiniFilterUserDll.dll"), "NPSendMessage");
+		printf("USER: LoadNPminifilterDll, pNPSendMessage: %p\n", pNPSendMessage);
+		if (!pNPSendMessage) {
+			return false;
+		}
+		return true;
+	}
+	return false;
+}
+
+void CNPApp::NPMessage(COMMAND_MESSAGE data)
+{
+	if (m_hModule == NULL) {
+		if (LoadNPminifilterDll() == false) {
+			return;
+		}
+	}
+	printf("USER: NPMessage\n");
+	pNPSendMessage((PVOID)(&data));
+}
+
+void main(void)
+{
+	CNPApp ControlObj;
+	char input;
+	while (true) {
+		cout << "Enter 'a' for PASS MODE, 'b' for BLOCKMODE or 'q' to EXIT" << endl;
+		cin >> input;
+		if (input == 'a' || input == 'A') {
+			COMMAND_MESSAGE data;
+			data.Command = ENUM_PASS;
+			ControlObj.NPMessage(data);
+			printf("==>NOTEPAD.EXE PASS MODE\n");
+		}
+		else if (input == 'b' || input == 'B') {
+			COMMAND_MESSAGE data;
+			data.Command = ENUM_BLOCK;
+			ControlObj.NPMessage(data);
+			printf("==>NOTEPAD.EXE BLOCK MODE\n");
+		}
+		else if (input == 'q' || input == 'Q') {
+			printf("EXIT\n");
+			break;
+		}
+		else {
+			printf("Wrong Parameter!!!\n");
+		}
+	};
+
+	system("pause");
+}
