2c6ade4826b4cba40dcaed729c66f78dc14335d3
Eternal <34774347+eternal2056@users.noreply.github.com>
2024-01-24 21:41:28 +0800

    [NEW] MFC

---
+++ MFCProject/MFCProject.h
@@ -0,0 +1,32 @@
+
+// MFCProject.h : main header file for the PROJECT_NAME application
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "include 'pch.h' before including this file for PCH"
+#endif
+
+#include "resource.h"		// main symbols
+
+
+// CMFCProjectApp:
+// See MFCProject.cpp for the implementation of this class
+//
+
+class CMFCProjectApp : public CWinApp
+{
+public:
+	CMFCProjectApp();
+
+// Overrides
+public:
+	virtual BOOL InitInstance();
+
+// Implementation
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMFCProjectApp theApp;
